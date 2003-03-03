d8800da7bc38b4ba9c4497d8e129f777082a52f4
Sebastian Bauer <mail@sebastianbauer.info>
2003-03-03 11:53:22 +0000

    Removed comments

--- source/GraphicsData.java
+++ source/GraphicsData.java
@@ -32,25 +32,6 @@ public class GraphicsData
 		3,22,
 		3,14,
 		6,5
-
-
-
-/* 			-13,40,
-			8,40,
-			9,25,
-			24,-10,
-			26,-12,
-			26,-14,
-			19,-45,
-			11,-64,
-			9,-66,
-			2,-70,
-			-1,-70,
-			-7,-66,
-			-17,-46,
-			-20,-19,
-			-13,19,
-*/
 	};
 	
 	static private int ladyHeelX = 0;
@@ -65,20 +46,10 @@ public class GraphicsData
 		5,30,
 		5,23,
 		3,22,
-
-
-/*			-13,42,
-			-12,65,
-			-3,70,
-			2,70,
-			10,62,
-			10,48,
-			7,42,*/ 
 	};
 
 
 	/* 25cm hoch */
-	
 	static private int gentBaleX = 0;
 	static private int gentBaleY = -29; 
 	static private int [] gentBale = new int []
@@ -104,22 +75,6 @@ public class GraphicsData
 		9,8,
 		8,11,
 		8,14
-
-		
-/*			-14,27,
-			-20,24,
-			-22,-4,
-			-30,-24,
-			-30,-48,
-			-25,-66,
-			-14,-87,
-			6,-87,
-			17,-79,
-			27,-59,
-			29,-39,
-			29,-31,
-			22,22,
-			11,27,*/
 	};
 	
 	static private int gentHeelX = 0;
@@ -138,71 +93,7 @@ public class GraphicsData
 		-8,37,
 		-10,35,
 		-12,30
-
-
-/*			-20,27,
-			11,27,
-			14,25,
-			22,25,
-			22,59,
-			15,71,
-			1,76,
-			-1,76,
-			-14,72,
-			-22,62*/
 	};
-
-	/*static {
-		int minx = 0xfff;
-		int maxx = 0;
-
-		int miny = 0xfff;
-		int maxy = 0;
-
-		for (int i=0;i<gentBale.length;i+=2)
-		{
-			int x = gentBale[i];
-			int y = gentBale[i+1];
-			
-			if (x<minx) minx = x;
-			if (x>maxx) maxx = x;
-
-			if (y<miny) miny = y;
-			if (y>maxy) maxy = y;
-		}
-
-		for (int i=0;i<gentHeel.length;i+=2)
-		{
-			int x = gentHeel[i];
-			int y = gentHeel[i+1];
-			
-			if (x<minx) minx = x;
-			if (x>maxx) maxx = x;
-
-			if (y<miny) miny = y;
-			if (y>maxy) maxy = y;
-		}
-
-		System.out.println(maxx - minx + 1);		
-		System.out.println(maxy - miny + 1);
-		
-		for (int i=0;i<gentBale.length;i+=2)
-		{
-			System.out.print(gentBale[i] - maxx + (maxx - minx + 1)/2);
-			System.out.print(",");
-			System.out.println(gentBale[i+1] - maxy + (maxy - miny + 1)/2);
-		}
-		
-		System.out.println();
-
-		for (int i=0;i<gentHeel.length;i+=2)
-		{
-			System.out.print(gentHeel[i] - maxx + (maxx - minx + 1)/2);
-			System.out.print(",");
-			System.out.println(gentHeel[i+1] - maxy + (maxy - miny + 1)/2);
-		}
-	}*/
-
 
 	public int baleX;
 	public int baleY;
