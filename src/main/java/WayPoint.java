39598425b0090d275e9965c3735f001674669c8c
Sebastian Bauer <mail@sebastianbauer.info>
2014-09-11 19:25:57 +0200

    Documented class.

--- src/main/java/WayPoint.java
+++ src/main/java/WayPoint.java
@@ -1,10 +1,7 @@
 /**
- * @author Standard
+ * A waypoint consists of a location and an angle. 
  *
- * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
- * To enable and disable the creation of type comments go to
- * Window>Preferences>Java>Code Generation.
+ * @author Sebastian Bauer
  */
 public class WayPoint
 {
