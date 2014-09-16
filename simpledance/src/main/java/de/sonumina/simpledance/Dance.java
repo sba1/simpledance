51ce286cf74067eb85b094343479f4f98adaaf20
Sebastian Bauer <mail@sebastianbauer.info>
2014-09-16 23:05:05 +0200

    Added brief javadoc for the class.

--- simpledance/src/main/java/de/sonumina/simpledance/Dance.java
+++ simpledance/src/main/java/de/sonumina/simpledance/Dance.java
@@ -53,12 +53,9 @@ import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
 
 /**
- * @author sebauer
+ * This class implements the main view and controller.
  *
- * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
- * To enable and disable the creation of type comments go to
- * Window>Preferences>Java>Code Generation.
+ * @author Sebastian Bauer
  */
 public class Dance implements Runnable
 {
