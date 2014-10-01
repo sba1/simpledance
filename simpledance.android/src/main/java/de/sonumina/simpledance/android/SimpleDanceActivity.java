4145a5fb2c8f5a0a018afb13b6b5f9030bbff104
Sebastian Bauer <mail@sebastianbauer.info>
2014-10-01 18:11:16 +0200

    Initial android package.
    Use
      mvn package -pl :simpledance.android -am -P Android
    in simpledance.parent to actually translate it. Note that the env
    variable ANDROID_HOME must be set. Also note that it is only a stub
    at the moment.

---
+++ simpledance.android/src/main/java/de/sonumina/simpledance/android/SimpleDanceActivity.java
@@ -0,0 +1,18 @@
+package de.sonumina.simpledance.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * The SimpleDance activity.
+ *
+ * @author Sebastian Bauer
+ */
+public class SimpleDanceActivity extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+}
