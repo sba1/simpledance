7b6594a7c642ea471ef2b9a59e352860e9e47fde
Sebastian Bauer <mail@sebastianbauer.info>
2014-10-11 20:30:58 +0200

    Initial attempt for Android support.
    The implementation of many methods is still missing.

---
+++ simpledance.android/src/main/java/de/sonumina/simpledance/android/AndroidContext.java
@@ -0,0 +1,127 @@
+package de.sonumina.simpledance.android;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import de.sonumina.simpledance.core.graphics.Color;
+import de.sonumina.simpledance.core.graphics.Context;
+import de.sonumina.simpledance.core.graphics.Point;
+import de.sonumina.simpledance.core.graphics.RGB;
+
+public class AndroidContext extends Context {
+	Canvas canvas;
+
+	public AndroidContext(Canvas canvas) {
+		this.canvas = canvas;
+	}
+
+	@Override
+	public void putPixel(int x, int y) {
+	}
+
+	@Override
+	public void drawPolyline(int[] data) {
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(android.graphics.Color.BLUE);
+        canvas.drawPaint(paint);
+	}
+
+	@Override
+	public void drawPolygon(int[] data) {
+		Paint paint = new Paint();
+		paint.setStyle(Paint.Style.FILL);
+		paint.setColor(android.graphics.Color.BLACK);
+
+		Path p = new Path();
+		p.reset();
+		p.moveTo(data[0], data[1]);
+		for (int i=2; i < data.length; i+=2)
+			p.lineTo(data[i], data[i+1]);
+		p.close();
+		canvas.drawPath(p, paint);
+	}
+
+	@Override
+	public void fillPolygon(int[] data) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void drawLine(int x0, int y0, int x1, int y1) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void drawOval(int x, int y, int mx, int my) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void drawText(String string, int x, int y, boolean transparent) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public Point stringExtent(String string) {
+		// TODO Implement this correctly.
+		return new Point(1,1);
+	}
+
+	@Override
+	public void gradientPolygon(int[] newData, RGB startRGB, RGB endRGB,
+			int angle) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public Color allocateColor(int r, int g, int b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deallocateColor(Color color) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void applyRotateTransformation(float angle) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void applyTranslationTransformation(float x, float y) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void applyScaleTransformation(float scale) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void applyScaleXTransformation(float f) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void printCurrentTransform() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void pushCurrentTransform() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void popCurrentTransform() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void applyTransformation(int[] sourcePointArray, int[] destPointArray) {
+		// TODO Auto-generated method stub
+	}
+}
--- simpledance.android/src/main/java/de/sonumina/simpledance/android/SimpleDanceActivity.java
+++ simpledance.android/src/main/java/de/sonumina/simpledance/android/SimpleDanceActivity.java
@@ -1,7 +1,15 @@
 package de.sonumina.simpledance.android;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import de.sonumina.simpledance.core.graphics.Point;
+import de.sonumina.simpledance.core.graphics.Render;
+import de.sonumina.simpledance.core.graphics.Render.RenderSceneArgs;
+import de.sonumina.simpledance.core.model.Pattern;
 
 /**
  * The SimpleDance activity.
@@ -10,9 +18,65 @@ import android.os.Bundle;
  */
 public class SimpleDanceActivity extends Activity
 {
+	static Pattern pattern = new Pattern();
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		setContentView(new BallroomView(this));
+	}
+
+	public class BallroomView extends View
+	{
+		int visibleLeft;
+		int visibleTop;
+		int zoomFactor = 100;
+		int rotation;
+
+		public BallroomView(Context context)
+		{
+			super(context);
+		}
+
+		private RenderSceneArgs getRenderSceneArgs()
+		{
+			RenderSceneArgs rsa = new RenderSceneArgs();
+			rsa.pattern = pattern;
+			rsa.stepNumber = pattern.getCurrentStepNum();
+			int visibleWidth = getWidth() * 100 / zoomFactor;
+			int visibleHeight = getHeight() * 100 / zoomFactor;
+			rsa.visibleLeftTop = new Point(visibleLeft, visibleTop);
+			rsa.visibleRightTop = rsa.visibleLeftTop.add(new Point(visibleWidth, 0).rotate(rotation));
+			rsa.visibleLeftBottom = rsa.visibleLeftTop.add(new Point(0, -visibleHeight).rotate(rotation));
+			rsa.visibleRightBottom = rsa.visibleLeftTop.add(new Point(visibleWidth, -visibleHeight).rotate(rotation));
+			rsa.visibleRotation = rotation;
+			rsa.pixelWidth = getWidth();
+			rsa.pixelHeight = getHeight();
+			rsa.showGent = true;
+			rsa.showLady = true;
+			return rsa;
+		}
+
+		@Override
+		protected void onDraw(Canvas canvas)
+		{
+			super.onDraw(canvas);
+
+			Paint paint = new Paint();
+			paint.setStyle(Paint.Style.FILL);
+			paint.setColor(android.graphics.Color.BLUE);
+			canvas.drawPaint(paint);
+
+			AndroidContext context = new AndroidContext(canvas);
+			Render render = new Render(context);
+
+			Render.ViewWholePatternResult result = render.viewWholePattern(getRenderSceneArgs());
+			visibleLeft = result.visibleLeft;
+			visibleTop = result.visibleTop;
+			zoomFactor = result.zoomFactor;
+
+			render.renderScence(getRenderSceneArgs());
+		}
 	}
 }
